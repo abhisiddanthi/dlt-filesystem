1207fc6be540e1d54b9d35a1da30e6dd24c64ca1
abhisiddanthi <abhiramsiddanthi@gmail.com>
2025-05-20 11:22:17 +0530

    Encoder Template

---
+++ Encoder/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "./Proto/logger_proto.pb.h"
+
+using namespace std;
+
+int main() {
+    //Code to take input from JSON and put into Log Class
+    
+    
+    //Code to encode the log object to string (Should be 1-2 lines max)
+    
+    //Code to log that one line onto a dlt file 
+    //(Couldn't find cpp library for that might have to use python for the POC)
+    //Or can do a fake call to aralogger but still need to use some library for POC
+
+
+    //Final dlt file should have all Json objects in the input.json file
+
+    return 0;
+}
